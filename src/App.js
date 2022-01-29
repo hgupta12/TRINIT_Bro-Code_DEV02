240cf80a5fc9d372c54230751b742e584ee2cbdd
hgupta12 <harshitgupta1406@gmail.com>
2022-01-30 00:46:58 +0530

    added signin and signup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+// import SignUpBox from "./components/SignUpBox";
+import SignInBox from "./components/SignInBox";
 
 
 function App() {
   return (
     <div className="App">
+      <SignInBox/>
+
     </div>
   );
 }
