847587fc89e5a3ef6d3b2e39903cf1dfd6b6b9cc
csking101 <42250012+csking101@users.noreply.github.com>
2022-01-29 21:04:47 +0530

    Added Issue Description and Discussion Component

---
+++ src/components/Comment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Comment() {
+  return <div>
+    <img />
+    <h3>Test UserName</h3>
+    <h4>Comment..</h4>
+    <h4>Time</h4>
+  </div>;
+}
+
+export default Comment;
--- src/components/IssueDescription.js
+++ src/components/IssueDescription.js
@@ -3,13 +3,13 @@ import React from 'react';
 
 function IssueDescription() {
 
-    const issue_id;//Takes in the int
-    const issue_title;//str
-    const description;//str
-    const status;//Assigned to whom
-    const raised_by;//str
-    const created_at;//convert datetime to str beforehand
-    const tags;//array of strings
+    const issue_id =1;//Takes in the int
+    const issue_title = "Test Title";//str
+    const description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";//str
+    const status = "";//Assigned to whom
+    const raised_by ="Test Raiser";//str
+    const created_at = "23:06:59 29/01/2022";//convert datetime to str beforehand
+    const tags = ["ui","frontend"];//array of strings
 
   return <div>
       <h1><u>#{issue_id}:{issue_title}</u></h1>
@@ -17,7 +17,7 @@ function IssueDescription() {
       <h2>{(status) === ""? "Unassigned":"Assigned to "+status}</h2>
       <h2>Raised by: {raised_by}</h2>
       <h2>Created At:{created_at}</h2>
-      <h2>Tags: {...tags}</h2>
+      
 
   </div>;
 }
