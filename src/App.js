b6ebc7263bf1cd37cc911197778edc73dfce1e16
csking101 <42250012+csking101@users.noreply.github.com>
2022-01-30 00:25:43 +0530

    Created the NavBar and its CSS
    The hover animation is incomplete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import './App.scss';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
     </div>
   );
 }
--- src/store/team-context.js
+++ src/store/team-context.js
@@ -1,6 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const initalState = {
+const initialState = {
     name:"",
     members:{},
     creator:{},
