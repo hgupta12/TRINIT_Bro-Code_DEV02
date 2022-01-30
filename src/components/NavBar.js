e10158b1c89a601176cf262761d0c351602915d9
hgupta12 <harshitgupta1406@gmail.com>
2022-01-30 13:37:57 +0530

    created .env file , add logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import {useSelector,useDispatch} from 'react-redux';
 import {auth} from '../firebase-config';
 import {signOut} from 'firebase/auth';
 import { userActions } from '../store/user-context';
+import Logo from '../images/logo.png'
 
 function NavBar() {
   const dispatch = useDispatch();
@@ -17,7 +18,7 @@ function NavBar() {
   }
   return (<div className="navbar">
     <ul id="navbar-list">
-      <li className="navbar-list-element"><Link to='/'>Logo</Link></li>
+      <li className="navbar-list-element"><Link to='/'><img src={Logo} alt="logo" /></Link></li>
       <li className="navbar-list-element"><Link to="/"><i className="fas fa-home"></i>Home</Link></li>
       <li className="navbar-list-element"><Link to="/"><i className="fas fa-network-wired"></i>Organisation</Link></li>
 
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoNQ2MNp-q9vSPb7ZDYBHr2G76pZxIzfg",
-  authDomain: "grasshopper-2f895.firebaseapp.com",
-  projectId: "grasshopper-2f895",
-  storageBucket: "grasshopper-2f895.appspot.com",
-  messagingSenderId: "236872774571",
-  appId: "1:236872774571:web:c41bbeafa227fd562bd8ee",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
 };
 
 // Initialize Firebase
